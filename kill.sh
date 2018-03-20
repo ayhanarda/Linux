3b72cccd3021bac5b333ab611e1efa39aef3beb6
ayhanarda <ayhanardaistanbul@gmail.com>
2018-03-20 22:07:48 +0300

    Update kill.sh

--- kill.sh
+++ kill.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-#ayhan arda - cron ile her dakika kontrol eder , mining vs. var ise kapatir
+#ayhan arda - cron ile her dakika kontrol eder , mining var ise kapatir
 if ps aux | grep -e 'byte' -e 'xmcc' -e 'cxmc' -e 'xmrigMiner' -e 'stratum' -e 'sqlmap.py' | grep -v grep > /dev/null
 then
-  ps aux | grep -e 'byte' -e 'xmcc' -e 'cxmc' -e 'xmrigMiner' -e 'stratum' -e 'sqlmap.py' | grep -v grep >> /root/silme/zararli.txt && kill -9 `ps ax | grep -e 'byte' $
+  ps aux | grep -e 'byte' -e 'xmcc' -e 'cxmc' -e 'xmrigMiner' -e 'stratum' -e 'sqlmap.py' | grep -v grep >> /root/silme/zararli.txt && kill -9 `ps ax | grep -e 'byte' -e 'xmcc' -e 'cxmc' -e 'xmrigMiner' -e 'stratum' -e 'sqlmap.py' | grep -v grep | awk '{print $1}'`
 else
 echo "sorun yok"
 fi
-
