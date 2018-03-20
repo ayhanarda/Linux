23224742c90073c4aa5c279ba8bc3573d5cf8933
ayhanarda <ayhanardaistanbul@gmail.com>
2018-03-20 21:57:48 +0300

    Update kill.sh

--- kill.sh
+++ kill.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #ayhan arda - cron ile her dakika kontrol eder , mining var ise kapatir
-if ps aux | grep stratum | grep tcp > /dev/null
+if ps aux | grep -e 'byte' -e 'xmcc' -e 'cxmc' -e 'xmrigMiner' -e 'stratum' | grep -v grep > /dev/null
 then
-  ps aux | grep stratum | grep tcp >> /root/silme/zararli.txt && kill -9 `ps ax | grep -v grep | grep stratum | awk '{print $1}'`
+  ps aux | grep -e 'byte' -e 'xmcc' -e 'cxmc' -e 'xmrigMiner' -e 'stratum' | grep -v grep >> /root/silme/zararli.txt && kill -9 `ps aux | grep -e 'byte' -e 'xmcc' -e 'cxmc' -e 'xmrigMiner' -e 'stratum' | grep -v grep | awk '{print $1}'`
 else
 echo "sorun yok"
 fi
